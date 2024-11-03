f9f10c866e3d6c93a025e14d6b0d01d3595b37b4
alfndp <alfndp25@gmail.com>
2024-11-03 21:43:25 +0900

    ref 와 portal 활용 연습

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,22 +1,32 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
 
-// 이렇게 하면 ref와 forwardRef를 사용해서 modal을 open 할 수 있다.
-// (* ResultModal에 직접 open 속성을 추가하는 방법도 있지만,)
-// (* 그렇게 하면 모달 뒤에 백그라운드 불투명 블랙배경이 자동으로 나오지 않음)
-//
-// 근데 이렇게 하면 개발자가 여러명인 경우에, dialog 를 div로 바꾼다거나, ref 명을 수정하는 등의 경우에
-// 참조가 깨지는 등의 에러가 날 수 있어서, useImperativeHandle 이라는 훅으로 관리해줘야 함.
-// 이 부분은 내일 이어서...
-const ResultModal =  forwardRef(function ResultModal({ result, targetTime }, ref) {
-  return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+const ResultModal =  forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
-      <p>You stopped the timer with <strong>X seconds left.</strong></p>
-      <form method="dialog">
+      <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   );
 })
 
